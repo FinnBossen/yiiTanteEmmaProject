827456654e50e8e201012284fe3204057e6a7bcb
Finn Bossen <finn.bossen.fb@gmail.com>
2020-08-28 00:41:02 +0200

    can only create articles if you are logged in

--- views/article/view.php
+++ views/article/view.php
@@ -15,28 +15,26 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <p class="text-muted">
+        <small>Created At: <b><?php echo Yii::$app->formatter->asRelativeTime($model->created_at); ?></b>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'slug',
-            'body:ntext',
-            'created_at',
-            'updated_at',
-            'created_by',
-        ],
-    ]) ?>
+            <b>By: <?php echo $model->createdBy->username ?></b>
+        </small>
+    </p>
+    <?php if (!Yii::$app->user->isGuest) : ?>
+        <p>
+            <?= Html::a('Update', ['update', 'slug' => $model->slug], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Delete', ['delete', 'slug' => $model->slug], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+    <?php endif; ?>
+    <div>
+        <?php echo $model->getEncodedBody() ?>
+    </div>
 
 </div>
